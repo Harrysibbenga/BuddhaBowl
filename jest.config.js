061e31b0df2785c42ff053907247e7bee2174a6d
Harry Sibbenga <sibbengaharry@gmail.com>
2023-12-05 14:35:47 +0000

    Updated jest config

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,13 @@
 module.exports = {
-    preset: "ts-jest",
-    testEnvironment: "jsdom",
-    moduleFileExtensions: ["js", "vue"],
-    moduleNameMapper: {
-      "^@/(.*)": "<rootDir>/$1",
-    },
+    testEnvironment: 'jest-environment-jsdom',
+    moduleFileExtensions: ['js', 'vue'],
     transform: {
-      "^.+\\.(js)$": "babel-jest",
-      ".+\\.(css|scss|png|jpg|svg)$": "jest-transform-stub",
-      ".*\\.(vue)$": "@vue/vue3-jest",
+      '^.+\\.vue$': 'vue-jest',
+      '^.+\\.js$': 'babel-jest',
     },
-    transformIgnorePatterns: ["node_modules/(?!(nuxt3|unenv))"],
+    moduleNameMapper: {
+      "^@/(.*)$": "<rootDir>/src/$1"
+    },
+    // If you have a setup file, specify it here
+    // setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
   };
