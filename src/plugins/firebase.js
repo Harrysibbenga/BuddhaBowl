f091c90b2df03973238506a7180ce53c0cd554cf
Harry Sibbenga <sibbengaharry@gmail.com>
2023-12-05 14:27:05 +0000

    Added jest and firebase

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -1,8 +1,11 @@
-import { initializeApp } from 'firebase/app'
-import { getFirestore, collection } from 'firebase/firestore'
+import { initializeApp, getApps } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { getAnalytics } from 'firebase/analytics';
 // ... other firebase imports
 
-export const firebaseApp = initializeApp({
+// Firebase configuration
+const firebaseConfig = {
     apiKey: process.env.VUE_APP_API_KEY,
     authDomain: process.env.VUE_APP_AUTH_DOMAIN,
     projectId: process.env.VUE_APP_PROJECT_ID,
@@ -10,10 +13,26 @@ export const firebaseApp = initializeApp({
     messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
     appId: process.env.VUE_APP_APP_ID,
     measurementId: process.env.VUE_APP_MEASUREMENT_ID
-})
+};
 
-// used for the firestore refs
-const db = getFirestore(firebaseApp)
+// Initialize Firebase only if it hasn't been initialized yet
+let firebaseApp;
+if (!getApps().length) {
+    firebaseApp = initializeApp(firebaseConfig);
+    // Initialize Firebase Analytics if it's not a SSR (Server Side Rendered) context
+    if (typeof window !== 'undefined') {
+        getAnalytics(firebaseApp);
+    }
+} else {
+    firebaseApp = getApps()[0]; // if already initialized, use that one
+}
 
-// here we can export reusable database references
-// export const todosRef = collection(db, 'todos')
+// Initialize Firestore and Auth
+const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
+
+// Export the necessary Firebase services
+export { firebaseApp, db, auth };
+
+// Example of exporting a reusable database reference
+// export const todosRef = collection(db, 'todos');
